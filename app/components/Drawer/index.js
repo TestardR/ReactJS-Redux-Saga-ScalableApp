34e93dc017076acbb6311b7cb2ab645c4fa66eab
Romain <rmtestard@gmail.com>
2018-11-20 17:28:54 +0100

    creating the Drawer Component

---
+++ app/components/Drawer/index.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * Drawer
+ *
+ */
+
+import React from 'react';
+
+import styles from './styles.css';
+
+function Drawer({
+  items,
+  selectiItem,
+  itemLabelAttr,
+  itemKeyAttr,
+  isDrawerOpen
+}) {
+  const itemNodes = items.map(item => (
+    <div
+      className={styles.item}
+      key={item[itemKeyAttr]}
+      onClick={() => selectiItem(item)}
+    >
+      {item[itemLabelAttr]}
+    </div>
+  ));
+  return <div className={styles.drawer}>{itemNodes}</div>;
+}
+
+Drawer.propTypes = {
+  items: React.PropTypes.arr.isRequired,
+  selectItem: React.PropTypes.func.isRequired,
+  itemLabelAttr: React.PropTypes.string.isRequired,
+  itemKeyAttr: React.PropTypes.string.isRequired,
+  isDrawerOpen: React.PropTypes.bool.isRequired
+};
+
+export default Drawer;
