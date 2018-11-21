a99331c56270230c7cab0d355da16ce556258c66
Romain <rmtestard@gmail.com>
2018-11-21 17:17:51 +0100

    Showing a Newly Created Object Inside the App

--- app/containers/LinkFormContainer/actions.js
+++ app/containers/LinkFormContainer/actions.js
@@ -4,11 +4,37 @@
  *
  */
 
-import { ADD_LINK } from './constants';
+import {
+  ADD_LINK,
+  ADD_LINK_FAILED,
+  ADD_LINK_SUCCESS,
+  ADD_LINK_CANCELLED
+} from './constants';
+
+export function addLinkCancelled() {
+  return {
+    type: ADD_LINK_CANCELLED
+  };
+}
 
 export function addLink(link) {
   return {
     type: ADD_LINK,
     link
+  };
+}
+
+export function addLinkSuccess(link) {
+  return {
+    type: ADD_LINK_SUCCESS,
+    link
+  };
+}
+
+export function addLinkFailed(link, message) {
+  return {
+    type: ADD_LINK_FAILED,
+    link,
+    message
   };
 }
