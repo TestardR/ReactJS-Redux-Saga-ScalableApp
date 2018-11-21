e072f3736cec72527ebb43f651549e082af3beba
Romain <rmtestard@gmail.com>
2018-11-21 12:57:44 +0100

    laying out a form

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -11,7 +11,24 @@ import styles from './styles.css';
 class Login extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
   render() {
-    return <div className={styles.login}>This is the Login component</div>;
+    return (
+      <div className={styles.login}>
+        <div className={styles.heading}>Login with your email</div>
+        <input
+          className={styles.input}
+          placeholder="Your email"
+          ref={f => {
+            this.emailField = f;
+          }}
+          type="text"
+        />
+
+        <div className={styles.actionContainer}>
+          <div className={styles.button}>cancel</div>
+          <div className={styles.button}>log in</div>
+        </div>
+      </div>
+    );
   }
 }
 
